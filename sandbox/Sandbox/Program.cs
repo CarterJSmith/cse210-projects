d26517b023e9a673f0eae28ef4b0dd2c6302281c
CarterJSmith <smi24043@byui.edu>
2024-09-20 20:56:50 -0600

    added second hello world

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Hello Potato World!");
+        
         Console.WriteLine("Hello Sandbox World!");
     }
 }
