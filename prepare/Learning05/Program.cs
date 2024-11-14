455abbee61965c1fbf712bd53851177ebd175e05
CarterJSmith <smi24043@byui.edu>
2024-11-14 14:10:04 -0700

    added shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 4.0);
+        Console.WriteLine($"The area of the square is {square.GetArea()}");
+        Rectangle rectangle = new Rectangle("Blue", 5.0, 3.0);
+        Console.WriteLine($"The area of the rectangle is {rectangle.GetArea()}");
+        Circle circle = new Circle(6.0, "Green");
+        Console.WriteLine($"The area of the circle is {circle.GetArea()}");
     }
 }
